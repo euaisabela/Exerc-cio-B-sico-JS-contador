78b63be9cd868209823f67e5b9fa1201f0ab844d
Isabela <129691258+euaisabela@users.noreply.github.com>
2024-03-11 15:06:44 -0300

    Create contadorbásico.js

---
+++ contadorbásico.js
@@ -0,0 +1,13 @@
+//Exercicio Alura JavaScript
+//// Crie um contador que comece em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+// iniciei criando a variável e adicionando o número solicitado para começar 
+let contador = 10 ;
+// Criei um loop para executar enquanto o contador for maior ou igual a zero 
+while(contador >= 0){
+    console.log(contador);
+    contador --;
+}
+
+// no console.log será exibido o valor atual do contador no console 
+//Após isso , utilizei o --para que fosse diminuindo o valor do contador a cada passagem pelo loop ( iteração ) 
